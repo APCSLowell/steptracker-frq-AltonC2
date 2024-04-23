2cb33bda2decc6775bfe7ce54e65b8c9d4c9a2d9
AltonC2 <143027301+AltonC2@users.noreply.github.com>
2024-04-22 21:38:39 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,27 @@
 import java.util.ArrayList;
 public class StepTracker
 {
+ private int mS, tS;
+ private int tD, aD;
+ public StepTracker(int goal){
+minSteps = goal;
+ tS = 0;
+ tD = 0;
+ aD = 0;
+ }
+ public void addDailySteps(int steps){
+  tS+=steps;
+  tD++;
+  if ( steps>= mS)
+aD++;
+ }
+ public int activeDays(){
+  return aD;
+ }
+ public double averageSteps()
+ if(tD==0)
+ return 0.0;
+else
+ return (double)tS/tD;
  /* to be implemented here */
 } 
